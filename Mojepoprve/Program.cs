7fd940ee0d7f385fa83dc07be2fe92aa05b1076d
Šimon Bíža <bizas@seznam.cz>
2021-02-17 19:50:24 +0100

    Prvni commit

--- Mojepoprve/Program.cs
+++ Mojepoprve/Program.cs
@@ -10,6 +10,43 @@ namespace Mojepoprve
     {
         static void Main(string[] args)
         {
+            /*int X = 5;
+            double Y = 1.3;
+            string nazev = "magy je kokot na nE10 ";
+            double vysledek = X + Y;
+            if (X>5)
+            {
+                Console.WriteLine(nazev + vysledek);
+
+            }
+             else if (Y==1.2)
+            {
+                Console.WriteLine("hovno");
+
+            }
+            else 
+            { 
+                Console.WriteLine("Boxic je kokot "); 
+            }
+            for(int i=0; i<10; i++) 
+            {  
+                Console.WriteLine("*" +i); 
+            }*/
+            string znak = "";
+            while (znak != "stop") 
+            {
+                Console.WriteLine("Ahoj kokote, pis co chces.Krom stop.");
+                znak = Console.ReadLine();
+                Console.WriteLine("ty kokote ted jsi to posral a napsal jsi :" + znak);
+
+            }
+            Console.WriteLine("uz jsem y toho venku");
+
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
